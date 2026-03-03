f5f2fa8f93fb465e274326f3ce6dddf07d95ab97
Juanalfaros <juan.alfaro.s@live.cl>
2026-03-03 06:42:24 -0300

    chore: add sanity studio appId for deployments

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -4,5 +4,8 @@ export default defineCliConfig({
     api: {
         projectId: 'x97w0suy',
         dataset: 'production'
-    }
+    },
+    deployment: {
+        appId: 'sjmr39x6kd7w5qy8pmr91hoh',
+    },
 })
